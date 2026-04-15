61eda65d8f8b82c7941e06f407282fe9d69ddefe
KennaWarren <mskyberg@rogent.com>
2026-04-15 13:46:15 -0500

    Add files via upload

--- associate-store/src/App.jsx
+++ associate-store/src/App.jsx
@@ -5,19 +5,21 @@ import ShopPage from "./pages/ShopPage";
 import CartPage from "./pages/CartPage";
 import AdminPage from "./pages/AdminPage";
 import AdminLogin from "./components/AdminLogin";
+import SuggestionsPage from "./pages/SuggestionsPage";
 
 export default function App() {
-  const [page, setPage]             = useState("shop");
+  const [page, setPage]               = useState("shop");
   const [adminAuthed, setAdminAuthed] = useState(false);
 
   return (
     <StoreProvider>
-      <div style={{ minHeight: "100vh", background: "#F7F7F7", fontFamily: "'Helvetica Neue', Helvetica, Arial, sans-serif" }}>
+      <div style={{ minHeight:"100vh", background:"#F7F7F7", fontFamily:"'Helvetica Neue', Helvetica, Arial, sans-serif" }}>
         <Header page={page} setPage={setPage} />
         <main>
-          {page === "shop"  && <ShopPage setPage={setPage} />}
-          {page === "cart"  && <CartPage setPage={setPage} />}
-          {page === "admin" && (
+          {page === "shop"        && <ShopPage setPage={setPage} />}
+          {page === "cart"        && <CartPage setPage={setPage} />}
+          {page === "suggestions" && <SuggestionsPage />}
+          {page === "admin"       && (
             adminAuthed
               ? <AdminPage />
               : <AdminLogin onSuccess={() => setAdminAuthed(true)} />
